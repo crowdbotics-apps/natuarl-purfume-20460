ec15eed326c53107c6e433776950c13fa84e310d
Crowdbotics <team@crowdbotics.com>
2020-09-19 22:46:55 +0000

    Update models

--- backend/natuarl_purfume_20460/wsgi.py
+++ backend/natuarl_purfume_20460/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'natuarl_purfume_20460.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "natuarl_purfume_20460.settings")
 
 application = get_wsgi_application()
